219763e6eb0ce3723c55de8ae54577f80f724c8e
harveer singh <harveersingh@live.com>
2016-07-10 18:01:55 -0400

    adding source files that captures image from camer

--- opencv/sample/build_and_setup.sh
+++ opencv/sample/build_and_setup.sh
@@ -1,6 +1,7 @@
 # It assumes that opencv-3.1.* is installed on /usr/local/lib.
-# Just this script and it should build successfully.
+# Just run this script and it should build successfully.
+# usage: ./build_and_setup.sh <path_to_cpp> <output_file_name>
 
-g++ -I ./include -L /usr/local/lib -o displayImage ./displayImage.cpp -lopencv_core -lopencv_imgcodecs -lopencv_highgui
+g++ -I ./include -L /usr/local/lib -o $2 $1 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -lopencv_videoio
 
-export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
